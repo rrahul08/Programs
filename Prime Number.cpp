3a53ec6521c7b0b2817f613591bbfd05ea0d2bb7
rrahul08 <100457954+rrahul08@users.noreply.github.com>
2022-08-16 19:04:51 +0530

    Create Prime Number.cpp

---
+++ Prime Number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,count=0;
+cout<<"Enter a number:";
+cin>>n;
+for(int i=2;i<n;i++)
+{
+if(n%i==0)
+cout++;
+}
+if(count==2)
+{
+cout<<"Prime";
+}
+else{
+cout<<"Composite";
+}
+return 0;
+}
